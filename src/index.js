ed2c70d08e8e9a55542674d99a4f85b12f28bba7
Sébastien Duval <sduval@smartadserver.com>
2018-07-19 23:18:02 +0200

    draft: user connection working

--- src/index.js
+++ src/index.js
@@ -7,116 +7,73 @@ const {
   log
 } = require('cozy-konnector-libs')
 const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very usefull for
-  // debugging but very verbose. That is why it is commented out by default
-  // debug: true,
-  // activates [cheerio](https://cheerio.js.org/) parsing on each page
   cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
   json: false,
   // this allows request-promise to keep cookies between requests
-  jar: true
+  jar: true,
+  debug: true
 })
 
-const baseUrl = 'http://books.toscrape.com'
+const baseUrl = 'https://espaceclient.aprr.fr/aprr/Pages'
+const loginUrl = baseUrl + '/connexion.aspx'
+const billUrl = baseUrl + '/MaConsommation/conso_factures.aspx'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
 async function start(fields) {
   log('info', 'Authenticating ...')
   await authenticate(fields.login, fields.password)
   log('info', 'Successfully logged in')
   // The BaseKonnector instance expects a Promise as return of the function
-  log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-  log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)
 
-  // here we use the saveBills function even if what we fetch are not bills, but this is the most
-  // common case in connectors
-  log('info', 'Saving data to Cozy')
-  await saveBills(documents, fields.folderPath, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
-  })
 }
 
-// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
-function authenticate(username, password) {
+async function authenticate(username, password) {
+  let loginPage
+  try {
+    loginPage = await request({
+      url: loginUrl,
+      gzip: true,
+      headers: {
+        'Accept-language': 'fr'
+      }
+    })
+  } catch (error) {
+    log('info', JSON.stringify(error.error))
+    throw new Error(error.VENDOR_DOWN)
+  }
+
+  const hiddenFields = {}
+  loginPage('input[type="hidden"]').each(function(i, elt) {
+    hiddenFields[elt.attribs.name] = elt.attribs.value
+  })
+
+  const formFields = {
+    ...hiddenFields,
+    ctl00$PlaceHolderMain$TextBoxLogin: username,
+    ctl00$PlaceHolderMain$TextBoxPass: password,
+    'ctl00$PlaceHolderMain$ImageButtonConnection.x': 54,
+    'ctl00$PlaceHolderMain$ImageButtonConnection.y': 12
+  }
+
+  // let $ = await request({
+  //   url: loginUrl,
+  //   method: 'POST',
+  //   form: formFields
+  // })
+
+  // log('info', login)
   return signin({
-    url: `http://quotes.toscrape.com/login`,
+    url: loginUrl,
     formSelector: 'form',
-    formData: { username, password },
+    formData: formFields,
     // the validate function will check if
     validate: (statusCode, $) => {
-      // The login in toscrape.com always works excepted when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
+      if ($("#ctl00_plhCustomerArea_customerArea_LinkButtonSeDeconnecter").length === 1) {
         return true
       } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
         return false
       }
     }
   })
-}
-
-// The goal of this function is to parse a html page wrapped by a cheerio instance
-// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
-    $,
-    {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
-      amount: {
-        sel: '.price_color',
-        parse: normalizePrice
-      },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
-      },
-      filename: {
-        sel: 'h3 a',
-        attr: 'title',
-        parse: title => `${title}.jpg`
-      }
-    },
-    'article'
-  )
-  return docs.map(doc => ({
-    ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: '€',
-    vendor: 'template',
-    metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // usefull for debugging or data migration
-      importDate: new Date(),
-      // document version, usefull for migration after change of document structure
-      version: 1
-    }
-  }))
-}
-
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
 }
